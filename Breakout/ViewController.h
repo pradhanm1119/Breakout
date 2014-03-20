6443f0af3469764cd7d8bfb2726b3c5ac439cd6d
Manas Pradhan <manaspradhan@Manass-MacBook-Air.local>
2014-03-20 13:57:38 -0500

    Initial Commit

---
+++ Breakout/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Breakout
+//
+//  Created by Manas Pradhan on 3/20/14.
+//  Copyright (c) 2014 Manas Pradhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Breakout/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Breakout
+//
+//  Created by Manas Pradhan on 3/20/14.
+//  Copyright (c) 2014 Manas Pradhan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
