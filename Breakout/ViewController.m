fdfdf3d03459b943088ebb19511ea5db2aee5079
Manas Pradhan <manaspradhan@Manass-MacBook-Air.local>
2014-03-21 19:01:27 -0500

    seventh commit

--- Breakout/ViewController.m
+++ Breakout/ViewController.m
@@ -29,6 +29,7 @@
     CGFloat               rectX;
     CGFloat               rectY;
 }
+@property BlockView *tempBlock;
 @end
 
 @implementation ViewController
@@ -36,6 +37,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    for (int i = 0; i < 25; i++)
+    {
+        rectX = arc4random() % 280;
+        rectY = arc4random() % 200;
+
+        self.tempBlock = [[BlockView alloc] initWithFrame:CGRectMake(rectX, rectY, 40, 40)];
+        self.tempBlock.backgroundColor = [UIColor greenColor];
+        
+        [self.view addSubview:self.tempBlock];
+        if (i == 0)
+        {
+            blocks = [[NSMutableArray alloc]initWithObjects:self.tempBlock, nil];
+        }
+        else
+        {
+            [blocks addObject:self.tempBlock];
+        }
+    }
     
     dynamicAnimator           = [[UIDynamicAnimator alloc] initWithReferenceView:self.view];
     pushBehavior              = [[UIPushBehavior alloc] initWithItems:@[pelletView] mode:UIPushBehaviorModeInstantaneous];
@@ -67,6 +87,27 @@
     [dynamicAnimator addBehavior:collisionBehavior];
     [dynamicAnimator addBehavior:pushBehavior];
     [dynamicAnimator addBehavior:blockDynamicBehavior];
+    
+//    for (int i = 0; i < 10; i++)
+//    {
+//        rectX = arc4random() % 280;
+//        rectY = arc4random() % 200;
+//        
+//        self.tempBlock = [[BlockView alloc] initWithFrame:CGRectMake(rectX, rectY, 40, 40)];
+//        self.tempBlock.backgroundColor = [UIColor greenColor];
+//        
+//        //[self.view addSubview:self.tempBlock];
+//        
+//        if (i == 0)
+//        {
+//            blocks = [[NSMutableArray alloc]initWithObjects:self.tempBlock, nil];
+//        }
+//        else
+//        {
+//            [blocks addObject:self.tempBlock];
+//        }
+//        [self.view addSubview:self.tempBlock];
+//    }
 }
 
 - (IBAction)dragPaddle:(UIPanGestureRecognizer *)panGestureRecognizer
@@ -90,6 +131,24 @@
         [collisionBehavior removeItem:blockView];
         [blockView removeFromSuperview];
     }
+    
+//    if ([item2 isMemberOfClass:[BlockView class]])
+//    {
+//        [collisionBehavior removeItem:item2];
+//        [blocks removeObject:item2];
+//        //you can only remove a view from itʻs Superview
+//        [(BlockView*)item2 removeFromSuperview];
+//        [dynamicAnimator updateItemUsingCurrentState:item2];
+//    }
+    
+//    if ([item1 isMemberOfClass:[BlockView class]])
+//    {
+//        [collisionBehavior removeItem:item1];
+//        [blocks removeObject:item1];
+//        //you can only remove a view from itʻs Superview
+//        [(BlockView*)item2 removeFromSuperview];
+//        [dynamicAnimator updateItemUsingCurrentState:item1];
+//    }
 }
 
 
